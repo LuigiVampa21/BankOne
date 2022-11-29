538d473c19cf54fa33f9b04616925892cf9d990e
TEISSIER Nathan <luigivampa12@protonmail.ch>
2022-11-29 10:23:50 +0100

    ntn

--- src/components/help/HelpChatCard.vue
+++ src/components/help/HelpChatCard.vue
@@ -1,31 +1,62 @@
 <template>
-  <ion-card color="secondary">
+  <ion-card
+    class="ion-padding"
+    :style="
+      chat.by === 'user'
+        ? { backgroundColor: 'var(--ion-color-tertiary-shade)' }
+        : { backgroundColor: 'var(--ion-color-medium)' }
+    "
+  >
     <ion-row>
-      <ion-item color="secondary" class="ion-margin">
-        <ion-avatar slot="start">
-          <img
-            class="avatar"
-            alt="Silhouette of a person's head"
-            src="https://ionicframework.com/docs/img/demos/avatar.svg"
-          />
-        </ion-avatar>
-        <ion-label>
-          <ion-text> {{ chat.by }} </ion-text>
-        </ion-label>
-      </ion-item>
+      <img
+        class="avatar"
+        alt="Silhouette of a person's head"
+        src="https://ionicframework.com/docs/img/demos/avatar.svg"
+      />
+      <ion-label
+        :style="
+          chat.by === 'user'
+            ? { backgroundColor: 'var(--ion-color-tertiary-shade)' }
+            : { backgroundColor: 'var(--ion-color-medium)' }
+        "
+      >
+        <ion-text class="msg-content ion-padding ion-text-capitalize">
+          {{ chat.by }}
+        </ion-text>
+      </ion-label>
     </ion-row>
-    <ion-row>{{ chat.content }}</ion-row>
-    <ion-row>{{ chat.date }}</ion-row>
+    <ion-row class="ion-padding msg-content">{{ chat.content }}</ion-row>
   </ion-card>
+  <ion-row
+    class="ion-margin msg-content date"
+    :class="
+      chat.by === 'user'
+        ? 'ion-justify-content-end'
+        : 'ion-justify-content-start'
+    "
+  >
+    <ion-text color="medium">
+      {{ new Date(chat.date).toLocaleDateString() }}
+    </ion-text>
+  </ion-row>
 </template>
 
 <script>
 export default {
   props: ["chat"],
-  created() {
-    console.log(this.chat);
-  },
+  setup() {},
 };
 </script>
 
-<style></style>
+<style>
+.avatar {
+  width: 30px;
+  border-radius: 50%;
+}
+.msg-content {
+  color: white;
+}
+.date {
+  font-size: 11px;
+}
+</style>
