9741dbf7503f0fb6737a2c1a30878cd6172eb6c5
luigi <smokalarm@protonmail.ch>
2022-12-13 12:08:27 +0100

    finaliz loop txe

--- src/components/base/BaseItemRow.vue
+++ src/components/base/BaseItemRow.vue
@@ -8,7 +8,7 @@
           <ion-text class="ion-text-capitalize"
             >{{ txI?.type }} transfer</ion-text
           >
-          <ion-text color="medium" class="ion-text-uppercase"><h6>M. {{ beneficiary }}</h6></ion-text>
+          <ion-text color="medium" class="ion-text-uppercase"><h6>M. {{ txI.beneficiary_name }}</h6></ion-text>
         </div>
         <ion-row class="tx-amount ion-justify-content-end">
           <ion-text :color="txI?.inflow ? 'success' : 'danger'"> {{txI?.inflow ? '+' : '-'}} {{txI?.amount}}€</ion-text> 
--- src/pages/TransactionsPage.vue
+++ src/pages/TransactionsPage.vue
@@ -1,6 +1,6 @@
 <template>
   <ion-page>
-    <base-layout :title="'Transactions'" :backLink="'/home'">
+    <base-layout :title="'Transactions'" :loading="loading" :backLink="'/home'">
       <ion-row class="ion-justify-content-end ion-margin-bottom">
         <ion-button
           color="tertiary"
@@ -14,16 +14,6 @@
         <ion-row class="ion-justify-content-center">
           <ion-row class="ion-margin-bottom"> <TransactionSearchBar /></ion-row>
           <ion-row class="transactions-container" v-if="allTxsR">
-            <!-- <TransactionCard :data="Income" />
-            <TransactionCard :data="Outcome" />
-            <TransactionCard :data="Income" />
-            <TransactionCard :data="Outcome" />
-            <TransactionCard :data="Income" />
-            <TransactionCard :data="Outcome" />
-            <TransactionCard :data="Income" />
-            <TransactionCard :data="Outcome" /> -->
-
-            <!-- <base-item-row v-for="tx in allTxsR" :key="tx?.id" :tx="tx"></base-item-row> -->
             <base-item-row :tx="allTxsR.value"></base-item-row>
           </ion-row>
         </ion-row>
@@ -38,7 +28,6 @@ import { useTxStore } from "../stores/transactions"
 import {storeToRefs} from "pinia"
 
 import TransactionSearchBar from "@/components/transactions/TransactionSearchBar.vue";
-// import TransactionCard from "@/components/transactions/TransactionCard.vue";
 
 import Income from "../utils/transaction/income";
 import Outcome from "../utils/transaction/outcome";
@@ -48,7 +37,6 @@ import { useRouter } from "vue-router";
 export default {
   components: {
     TransactionSearchBar,
-    // TransactionCard,
     IonPage,
     IonRow,
     IonGrid,
@@ -56,7 +44,7 @@ export default {
   setup() {
     const router = useRouter();
     const txStore = useTxStore();
-    const {transactions} = storeToRefs(txStore);
+    const {transactions, loading} = storeToRefs(txStore);
     let allTxsR = ref(null);
     onMounted(() => {
      txStore.getAllTxs()
@@ -69,7 +57,8 @@ export default {
       Income,
       Outcome,
       router,
-      allTxsR
+      allTxsR,
+      loading
     };
   },
 };
