c9dda11c7465b5f43118d9cdcd4dfc5100236891
Nathan Teissier <nmteissier@gmail.com>
2023-03-24 14:48:21 +0100

    last commit

--- src/components/base/BaseLayout.vue
+++ src/components/base/BaseLayout.vue
@@ -174,7 +174,7 @@ export default defineComponent({
   height: 6.5vh;
   border-radius: 9999px;
   position: fixed;
-  top: 80vh;
+  top: 70vh;
   left: 50%;
   transform: translateX(-50%);
   z-index: 1;
